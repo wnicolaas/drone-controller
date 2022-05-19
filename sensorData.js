75f42556666b2327e5ddbcf4d7a1ff75dfe2582f
wnicolaas <wietsenicolaas@gmail.com>
2022-05-19 16:06:31 +0200

    Created sensordata class which holds sensor data values

--- sensorData.js
+++ sensorData.js
@@ -1,3 +1,50 @@
-class sensorData {
+class SensorData {
+    gyro;
+    ultrasonic;
+    temperature;
+    gps;
+    batteryPercentage;
+
+    set gyro(gyro) {
+        this.gyro = gyro;
+    }
+
+    set ultrasonic(us) {
+        this.ultrasonic = us;
+    }
+
+    set temperature(temp) {
+        this.temperature = temp;
+    }
+
+    set gps(gps) {
+        this.gps = gps;
+    }
+
+    set batteryPercentage(bat) {
+        this.batteryPercentage = bat;
+    }
+
+    get getGps() {
+        return this.gps;
+    }
+
+    get getTemperature() {
+        return this.temperature;
+    }
+
+    get getUltrasonicData() {
+        return this.ultrasonic;
+    }
+
+    get getGyroData() {
+        return this.gyro;
+    }
+
+    get getBatteryPercentage() {
+        return this.batteryPercentage;
+    }
 
 }
+
+module.exports = SensorData;
