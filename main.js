f3780a65f01ba7f96cf7b91925f7670c79c2f01f
wnicolaas <wietsenicolaas@gmail.com>
2022-05-19 16:07:08 +0200

    Updated main test file with increased event listener and method to retrieve sensor data

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const arDrone = require("ar-drone");
+const SensorData = require("./sensorData");
 
 const client = arDrone.createClient();
 
-let last = 0;
+const sensorData = new SensorData();
+
+client.config('general:navdata_demo', 'FALSE');
+client.config('general:navdata_options', 777060865);
 
 client.on('navdata', (data) => {
-    let now = new Date().getTime();
-    if(now - last > 3000) {
-        last = now;
+    if (data.demo) {
+        sensorData.batteryPercentage = data.demo.batteryPercentage;
+    }
 
-        if (data.demo) {
-            console.log('percentage: ' + data.demo.batteryPercentage);
-        }
+    if (data.rawMeasures) {
+        sensorData.gyro = data.rawMeasures.gyroscopes;
+    }
+
+    if (data.gps) {
+        sensorData.gps = data.gps;
     }
 });
+
+function retrieveAllData() {
+    console.log("Battery percentage: " + sensorData.batteryPercentage + '%');
+    console.log("Gyro X value: " + sensorData.gyro.x);
+    console.log("Gyro Y value: " + sensorData.gyro.y);
+    console.log("Gyro Z value: " + sensorData.gyro.z);
+    console.log("GPS latitude: " + sensorData.gps.latitude);
+}
+
+setTimeout(retrieveAllData, 2000);
+
+console.log('Hello');
