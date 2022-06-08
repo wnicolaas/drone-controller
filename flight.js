045c9c21b211e1bddb22cc41d5155b212e3dfd15
Tom <tomvandenberg12@gmail.com>
2022-06-08 11:41:13 +0200

    added rgbController

--- flight.js
+++ flight.js
@@ -119,19 +119,6 @@ function sleep(ms) {
     }))
 }
 
-// setInterval(function() {
-//     // The emergency: true option recovers your drone from emergency mode that can
-//     // be caused by flipping it upside down or the drone crashing into something.
-//     // In a real program you probably only want to send emergency: true for one
-//     // second in the beginning, otherwise your drone may attempt to takeoff again
-//     // after a crash.
-//     control.ref(ref);
-//     // This command makes sure your drone hovers in place and does not drift.
-//     control.pcmd(pcmd);
-//     // This causes the actual udp message to be send (multiple commands are
-//     // combined into one message)
-//     control.flush();
-// }, 30);
 
 setInterval(function() {
     client._ref = ref;
---
+++ rgbController.js
@@ -0,0 +1,32 @@
+var Gpio = require('pigpio').Gpio, //include pigpio to interact with the GPIO
+    ledRed = new Gpio(4, {mode: Gpio.OUTPUT}), //use GPIO pin 4 as output for RED
+    ledGreen = new Gpio(17, {mode: Gpio.OUTPUT}), //use GPIO pin 17 as output for GREEN
+    redRGB = 0, //set starting value of RED variable to off (0 for common cathode)
+    greenRGB = 0, //set starting value of GREEN variable to off (0 for common cathode)
+    blueRGB = 0; //set starting value of BLUE variable to off (0 for common cathode)
+var percentageBattery = 100;
+
+//RESET RGB LED
+ledRed.digitalWrite(0); // Turn RED LED off
+ledGreen.digitalWrite(0); // Turn GREEN LED off
+
+
+
+setInterval(function(){
+
+    redRGB = (255 * (100 - parseInt(percentageBattery))) / 100
+    greenRGB = (255 *parseInt(percentageBattery)) / 100
+
+    ledRed.pwmWrite(parseInt(redRGB)); //set RED LED to specified value
+    ledGreen.pwmWrite(parseInt(greenRGB)); //set GREEN LED to specified value
+
+},1000);
+
+
+process.on('SIGINT', function () { //on ctrl+c
+    ledRed.digitalWrite(0); // Turn RED LED off
+    ledGreen.digitalWrite(0); // Turn GREEN LED off
+    process.exit(); //exit completely
+});
+
+module.export.percentageBattery = percentageBattery;
